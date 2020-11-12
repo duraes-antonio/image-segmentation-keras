36f93fc086e8ec359827a27255c1358bfd313854
duraes-antonio <garotoseis@gmail.com>
2020-11-12 17:48:09 -0300

    FIX: Erro dropout VGG #1

--- keras_segmentation/models/vgg16.py
+++ keras_segmentation/models/vgg16.py
@@ -35,18 +35,18 @@ def get_vgg_encoder(
                name='block1_conv1', data_format=IMAGE_ORDERING)(img_input)
     x = Conv2D(64, (3, 3), activation='relu', padding='same',
                name='block1_conv2', data_format=IMAGE_ORDERING)(x)
+    x = Dropout(dropout_value if dropout else 0)(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool',
                      data_format=IMAGE_ORDERING)(x)
-    x = Dropout(dropout_value if dropout else 0)(x)
     f1 = x
     # Block 2
     x = Conv2D(128, (3, 3), activation='relu', padding='same',
                name='block2_conv1', data_format=IMAGE_ORDERING)(x)
     x = Conv2D(128, (3, 3), activation='relu', padding='same',
                name='block2_conv2', data_format=IMAGE_ORDERING)(x)
+    x = Dropout(dropout_value if dropout else 0)(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool',
                      data_format=IMAGE_ORDERING)(x)
-    x = Dropout(dropout_value if dropout else 0)(x)
     f2 = x
 
     # Block 3
@@ -56,9 +56,9 @@ def get_vgg_encoder(
                name='block3_conv2', data_format=IMAGE_ORDERING)(x)
     x = Conv2D(256, (3, 3), activation='relu', padding='same',
                name='block3_conv3', data_format=IMAGE_ORDERING)(x)
+    x = Dropout(dropout_value if dropout else 0)(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool',
                      data_format=IMAGE_ORDERING)(x)
-    x = Dropout(dropout_value if dropout else 0)(x)
     f3 = x
 
     # Block 4
@@ -68,9 +68,9 @@ def get_vgg_encoder(
                name='block4_conv2', data_format=IMAGE_ORDERING)(x)
     x = Conv2D(512, (3, 3), activation='relu', padding='same',
                name='block4_conv3', data_format=IMAGE_ORDERING)(x)
+    x = Dropout(dropout_value if dropout else 0)(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool',
                      data_format=IMAGE_ORDERING)(x)
-    x = Dropout(dropout_value if dropout else 0)(x)
     f4 = x
 
     # Block 5
@@ -80,9 +80,9 @@ def get_vgg_encoder(
                name='block5_conv2', data_format=IMAGE_ORDERING)(x)
     x = Conv2D(512, (3, 3), activation='relu', padding='same',
                name='block5_conv3', data_format=IMAGE_ORDERING)(x)
+    x = Dropout(dropout_value if dropout else 0)(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool',
                      data_format=IMAGE_ORDERING)(x)
-    x = Dropout(dropout_value if dropout else 0)(x)
     f5 = x
 
     if pretrained == 'imagenet':
